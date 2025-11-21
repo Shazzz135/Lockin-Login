e1815c53d911b2df25702fe97a42693cab78cc04
Shazzz <nickshahbaz135@gmail.com>
2025-11-20 23:39:02 -0500

    added count

--- backend/users.js
+++ backend/users.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   lastname: { type: String, required: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  count: { type: Number, default: 0 },
   datecreated: { type: Date, default: Date.now }
 });
 
